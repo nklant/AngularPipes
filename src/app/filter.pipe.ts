e6a04eeaa6dc2105c5e0aa08c918048684f45b68
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-03-20 16:25:05 +0200

    230. Pure and Impure Pipes (or: How to "fix" the Filter Pipe)

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false // might lead into performance issues if the list is too long
 })
 export class FilterPipe implements PipeTransform {
 
